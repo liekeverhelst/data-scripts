4c1980814b02af6d15beb9aa29f42e0a39afd21d
Lieke Verhelst <LiekeVerhelst@gmail.com>
2020-03-03 18:16:02 +0100

    initial commit

--- import-mongodb.py
+++ import-mongodb.py
@@ -3,3 +3,18 @@
 
 import os
 
+rootdir = input("geef directory: ")
+databasename = input("databasenaam: ")
+outfile = input("geef outputfile naam: ")
+f_out=open(outfile, "a")
+
+for dirpath, dirs, files in os.walk(rootdir): 
+    for filename in files:
+        fname = os.path.join(dirpath,filename).replace("\\","//")
+        if fname.endswith('.json'):
+            fshortname = os.path.splitext(filename)[0]
+            commandstr = "mongoimport --db " + databasename + " --collection "+  fshortname + " --file "+ fname + " --jsonArray --upsertFields=\"id\" " 
+            
+            f_out.write(commandstr)
+            f_out.write("\n")
+f_out.close()
