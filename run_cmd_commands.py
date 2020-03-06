d0ee1d89f6d7def6160c83d67bf41c75b6345443
Lieke Verhelst <LiekeVerhelst@gmail.com>
2020-03-06 17:08:05 +0100

    added counter

--- run_cmd_commands.py
+++ run_cmd_commands.py
@@ -9,11 +9,14 @@ filename=input("geef lijst met commands: ")
 
 with open(filename, encoding='utf-8') as f:
     cmdlist = f.read().splitlines()
+    count = 0
 
     for cmd in cmdlist:
         try:
             return_code = subprocess.call(cmd, shell=True)
-            print(return_code)
+            count =count +1
+            print("command # " + count + " completed with return code " + return_code)
+            
         
         except Exception as e:
             if hasattr(e, 'message'):
